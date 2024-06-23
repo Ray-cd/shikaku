73e1a391609ad2185c057562b854b465be65b4ef
chetan dashora <dashora.chetan@gamil.com>
2024-06-23 22:25:56 +0530

    [fix] add target branch

--- compare.go
+++ compare.go
@@ -15,24 +15,24 @@ var compareCmd = &cobra.Command{
 	Short: "Compare branch with master and check coding principles",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 2 {
-			log.Fatal("Usage: compare [repo_owner] [repo_name] [branch]")
+			log.Fatal("Usage: compare [repo_owner] [repo_name] [target] [branch]")
 		}
-		repoOwner, repoName, branch := args[0], args[1], args[2]
-		checkChanges(repoOwner, repoName, branch)
+		repoOwner, repoName, target, branch := args[0], args[1], args[2], args[3]
+		checkChanges(repoOwner, repoName, target, branch)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(compareCmd)
 }
-func checkChanges(owner string, repo string, branch string) {
+func checkChanges(owner string, repo string, target string, branch string) {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
-	compare, _, err := client.Repositories.CompareCommits(ctx, owner, repo, "master", branch, nil)
+	compare, _, err := client.Repositories.CompareCommits(ctx, owner, repo, target, branch, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
